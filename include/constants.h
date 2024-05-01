49233cac59d7c3fc3a96ea573af9c4caa366d5b0
JiMMiZ <jaumeguillot@gmail.com>
2024-05-01 21:13:28 +0200

    Added const for HEREDOC error

--- include/constants.h
+++ include/constants.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   constants.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jguillot <jguillot@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jguillot <jguillot@student.42barcelona>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 16:04:37 by sadoming          #+#    #+#             */
-/*   Updated: 2024/04/30 18:00:32 by jguillot         ###   ########.fr       */
+/*   Updated: 2024/05/01 21:12:53 by jguillot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,7 @@
 # define MERR_FORK "Fork failed"
 # define MERR_FILE "No such file or directory"
 # define MERR_UNSET "unset: error getting env var"
-# define MSG_CMD_NOT_FOUND	"command not found"
+# define MSG_CMD_NOT_FOUND "command not found"
+# define MSG_HEREDOC_ERR "cannot create temp file for here document"
+
 #endif
