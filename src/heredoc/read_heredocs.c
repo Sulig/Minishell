1e87a8c850b2d6ad8a1c2d88d7a48f2782514680
JiMMiZ <jaumeguillot@gmail.com>
2024-05-20 09:57:53 +0200

    added signals

--- src/heredoc/read_heredocs.c
+++ src/heredoc/read_heredocs.c
@@ -3,19 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   read_heredocs.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jguillot <jguillot@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jguillot <jguillot@student.42barcelona>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/24 16:07:20 by jguillot          #+#    #+#             */
-/*   Updated: 2024/05/14 19:09:55 by jguillot         ###   ########.fr       */
+/*   Updated: 2024/05/20 09:57:20 by jguillot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-// Creates a here document, reading the standard input until 'd' is found and
-// saving the content into the specified temp 'file'.
-// If 'x' is TRUE, the variable names are expanded.
-// Exits with 0 on success, 1 on SIGINT received, or -1 in case of failure.
+/*
+* Creates a here document, reading the standard input until 'd' is found and
+* saving the content into the specified temp 'file'.
+* If 'x' is TRUE, the variable names are expanded.
+* Exits with 0 on success, 1 on SIGINT received, or -1 in case of failure.
+*/
 static int	read_hd_child(char *d, const char *file, t_shell *tshell)
 {
 	int		fd_file;
@@ -29,31 +31,42 @@ static int	read_hd_child(char *d, const char *file, t_shell *tshell)
 	return tshell->exit_state;
 }
 
-// Creates a here document, reading the standard input until 'delim' is found
-// and saving the content into the specified temp 'file'.
-// Returns 0 on success, 1 on SIGINT received, or -1 in case of failure.
+/*
+* Creates a here document, reading the standard input until 'delim' is found
+* and saving the content into the specified temp 'file'.
+* Returns 0 on success, 1 on SIGINT received, or -1 in case of failure.
+* pid_t wait(int *status);-> It suspends execution of the calling process until
+* a child specified by pid argument has changed state
+* WIFEXITED and WEXITSTATUS are two of the options which can be used to know
+* the exit status of the child.
+* WIFEXITED(status) : returns true if the child terminated normally.
+* WEXITSTATUS(status) : returns the exit status of the child. This macro should be
+* employed only if WIFEXITED returned true.
+*/
 int	read_heredoc(char *delim, const char *file, t_shell *tshell)
 {
-	//pid_t	pid;
-	//int		status;
+	pid_t	pid;
+	int		status;
 
-	//pid = fork();
-	//if (pid == -1)
-	//	exit (EXIT_FAILURE);
-	//if (pid == 0)
+	pid = fork();
+	if (pid == -1)
+		exit (EXIT_FAILURE);
+	if (pid == 0)
 		read_hd_child(delim, file, tshell);
-	//wait(&status);
-	//if (WIFEXITED(status))
-	//	return (WEXITSTATUS(status));
-	//else if (WIFSIGNALED(status) && (WTERMSIG(status) == SIGINT))
-	//	return (EXIT_FAILURE);
-	//return (EXIT_FAILURE);
+	wait(&status);
+	if (WIFEXITED(status))
+		return (WEXITSTATUS(status));
+	else if (WIFSIGNALED(status) && (WTERMSIG(status) == SIGINT))
+		return (EXIT_FAILURE);
+	return (EXIT_FAILURE);
 	return (0);
 }
 
-// Reads the here document for the 'n'-th command, assuming 'str' is the
-// delimiter
-// Returns 0 on success, or -1 in case of failure
+/*
+* Reads the here document for the 'n'-th command, assuming 'str' is the
+* delimiter
+* Returns 0 on success, or -1 in case of failure
+*/
 static int	read_heredoc_n(char *str, int n, t_shell *tshell)
 {
 	char	*delim;
@@ -68,10 +81,12 @@ static int	read_heredoc_n(char *str, int n, t_shell *tshell)
 	return (exit_status);
 }
 
-// Reads the here documents for 'cmd' from left to right, into a temp file
-// knowing it's the 'n'-th command.
-// If there's more than one here document, the previous ones are overwritten.
-// Returns 0 on success. Otherwise, returns a non-zero value.
+/*
+* Reads the here documents for 'cmd' from left to right, into a temp file
+* knowing it's the 'n'-th command.
+* If there's more than one here document, the previous ones are overwritten.
+* Returns 0 on success. Otherwise, returns a non-zero value.
+*/
 int	read_heredocs(t_list *cmds, int n, t_shell *tshell)
 {
 	t_list	*node;
@@ -93,9 +108,11 @@ int	read_heredocs(t_list *cmds, int n, t_shell *tshell)
 	return (EXIT_SUCCESS);
 }
 
-// Reads all the here documents from the array of commands 'cmds', from left
-// to right, into the appropriate temp files to be read afterwards.
-// Returns 0 on success. Otherwise, returns a non-zero value.
+/*
+* Reads all the here documents from the array of commands 'cmds', from left
+* to right, into the appropriate temp files to be read afterwards.
+* Returns 0 on success. Otherwise, returns a non-zero value.
+*/
 int	read_all_heredocs(t_list **cmds, int cmds_amount, t_shell *tshell)
 {
 	int		i;
