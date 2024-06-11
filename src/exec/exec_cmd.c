5e50a47d6368bce91bae7f80a9d40a6978f306fe
JiMMiZ <jaumeguillot@gmail.com>
2024-06-11 17:59:51 +0200

    Added expr case

--- src/exec/exec_cmd.c
+++ src/exec/exec_cmd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exec_cmd.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jguillot <jguillot@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jguillot <jguillot@student.42barcelona>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/24 17:11:04 by jguillot          #+#    #+#             */
-/*   Updated: 2024/05/21 19:10:31 by jguillot         ###   ########.fr       */
+/*   Updated: 2024/06/11 17:54:58 by jguillot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,29 @@ char	**parse_args(char *path, char *args)
 	return (ft_strarrdup(arg));
 }
 
+char	**parse_expr(char *path, char *args)
+{
+	char	**splited;
+	char	**arg;
+	int		items;
+	int		i;
+
+	splited = ft_split(args, ' ');
+	items = ft_arr_strlen(splited);
+
+	arg = malloc(sizeof(char *)*(items + 2));
+	i = 1;
+	arg[0] = path;
+	while (i <= items)
+	{
+		arg[i] = ft_strdup(splited[i-1]);
+		i++;
+	}
+	arg[i] = NULL;
+	free_arr_2d(splited);
+	return (ft_strarrdup(arg));
+}
+
 // Executes the shell command 'cmd' and exits, assuming 'cmd' is a non-empty
 // NULL-terminated array of strings in which the first element is the path or
 // name of the program and the following (if any) are the arguments.
@@ -47,8 +70,10 @@ void	exec_cmd(t_cmd *cmd, char **env)
 		path = get_executable(path, env);
 	else
 		check_path(path);
-	args = parse_args(path, cmd->options);
-	args = parse_args(path, cmd->input);
+	if (!ft_strstr(path, "expr"))
+		args = parse_args(path, cmd->options);
+	else
+		args = parse_expr(path, cmd->input);
 	execve(path, args, env);
 	print_comandnotfound(cmd->comand);
 	exit(EXIT_FAILURE);
