225ddf117b204c2684d76b74dc3141d7ce240b32
Sasha Domínguez Ferrer <sadoming@student.42barcelona.com>
2024-06-18 16:17:27 +0200

    Do not include config

--- src/minishell_main.c
+++ src/minishell_main.c
@@ -6,7 +6,7 @@
 /*   By: sadoming <sadoming@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 19:21:54 by sadoming          #+#    #+#             */
-/*   Updated: 2024/06/17 19:24:49 by sadoming         ###   ########.fr       */
+/*   Updated: 2024/06/18 16:16:06 by sadoming         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ void	minishell(t_shell *tshell)
 			exit_minishell(tshell);
 		split_intotokens(tshell);
 		split_intocomands(tshell, tshell->tokens);
-		//print_comands_st(tshell->comands);
+		print_comands_st(tshell->comands);
 		tshell->line = ft_free_str(tshell->line);
 		split_intodoublelist(tshell);
 		free_tokens(tshell);
