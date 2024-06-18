a978c0ff48178488d06d7cd584eb1f96c735ceac
Sasha Domínguez Ferrer <sadoming@student.42barcelona.com>
2024-06-18 18:08:24 +0200

    Attempt 1 to eval

--- src/minishell_main.c
+++ src/minishell_main.c
@@ -6,7 +6,7 @@
 /*   By: sadoming <sadoming@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 19:21:54 by sadoming          #+#    #+#             */
-/*   Updated: 2024/06/18 17:36:01 by sadoming         ###   ########.fr       */
+/*   Updated: 2024/06/18 17:59:09 by sadoming         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,6 @@ void	minishell(t_shell *tshell)
 			exit_minishell(tshell);
 		split_intotokens(tshell);
 		split_intocomands(tshell, tshell->tokens);
-		print_comands_st(tshell->comands);
 		tshell->line = ft_free_str(tshell->line);
 		split_intodoublelist(tshell);
 		free_tokens(tshell);
